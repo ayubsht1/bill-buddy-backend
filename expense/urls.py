6642da6773b13cd6ccdbcb9a4791c5234421f050
Ayub Shrestha <136778245+ayubsht1@users.noreply.github.com>
2025-08-13 10:34:27 +0545

    Update urls.py
    added urls for expense

--- expense/urls.py
+++ expense/urls.py
@@ -1,9 +1,9 @@
+# expenses/urls.py
 from django.urls import path
-from django.http import HttpResponse
-
-def expense_text(request):
-    return HttpResponse("expense", content_type="text/plain")
+from .views import ExpenseListCreateView, ExpenseDetailView, ExpenseShareListView
 
 urlpatterns = [
-    path("expense/", expense_text),
+    path("groups/<int:group_id>/expenses/", ExpenseListCreateView.as_view(), name="expense-list-create"),
+    path("expenses/<int:pk>/", ExpenseDetailView.as_view(), name="expense-detail"),
+    path("expenses/<int:expense_id>/shares/", ExpenseShareListView.as_view(), name="expense-share-list"),
 ]
